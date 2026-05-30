fda993d630cab5ff47742a393015d09abf1aba47
JaivTheCoder <110473948+JaivTheCoder@users.noreply.github.com>
2026-05-30 11:18:11 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,6 @@ def home():
     return "Cricket Bot is alive and running!"
 
 def run():
-    # Render assigns a dynamic port. This grabs it safely!
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
